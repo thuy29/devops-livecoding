bd36bab9f0abcc10e9b0123fc54af6ef372a0419
Co <thuylinh.co@efrei.net>
2024-12-13 13:49:36 +0100

    modify

--- database/01-CreateSchema.sql
+++ database/01-CreateSchema.sql
@@ -1,11 +1,6 @@
-CREATE TABLE departments (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE students (
     id SERIAL PRIMARY KEY,
-    department_id INTEGER REFERENCES departments(id),
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL
+    name VARCHAR(255) NOT NULL,
+    department_id INT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
