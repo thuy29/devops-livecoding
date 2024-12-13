2d0b21786a403da081e9aab265f42b6773167558
Co <thuylinh.co@efrei.net>
2024-12-13 01:53:32 +0100

    Create Schema

--- ansible/inventories/roles/database/files/01-CreateSchema.sql
+++ ansible/inventories/roles/database/files/01-CreateSchema.sql
@@ -2,3 +2,10 @@ CREATE TABLE departments (
     id SERIAL PRIMARY KEY,
     name VARCHAR(255) NOT NULL
 );
+
+CREATE TABLE students (
+    id SERIAL PRIMARY KEY,
+    department_id INTEGER REFERENCES departments(id),
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL
+);
